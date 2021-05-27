8de7ff729a3f158479fa4f2cfb683d2f041e3b30
nicoram28 <nahuelramos.marset@gmail.com>
2021-05-27 10:43:36 -0300

    Ejercicio 1 completado

---
+++ src/CuentaCorriente.java
@@ -0,0 +1,40 @@
+public class CuentaCorriente {
+
+    private double saldo;
+
+    public CuentaCorriente() {
+    }
+
+    public CuentaCorriente(int saldo) {
+        this.saldo = saldo;
+    }
+
+    public CuentaCorriente(CuentaCorriente cuenta) {
+        this.saldo = cuenta.getSaldo();
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void ingreso(){
+
+    }
+
+    public void egreso(){
+
+    }
+
+    public void reitegro(){
+
+    }
+
+    public void transferencia(){
+
+    }
+
+}
---
+++ src/Ejercicios.java
@@ -0,0 +1,5 @@
+public class Ejercicios {
+
+    public static void main(String[] args) {
+    }
+}
